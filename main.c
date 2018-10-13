ea28831ce416ea216c8607ca8c96600b82ff1433
Tempurturtul <tempurturtul@gmail.com>
2018-10-13 12:43:57 -0400

    Add another color pair

--- main.c
+++ main.c
@@ -68,6 +68,7 @@ void startCurses() {
 	init_pair(1, COLOR_WHITE, COLOR_BLACK);
 	init_pair(2, COLOR_RED, COLOR_BLACK);
 	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(4, COLOR_CYAN, COLOR_BLACK);
 }
 
 void endCurses() {
